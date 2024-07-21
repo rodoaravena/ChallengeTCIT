2ca639adfee005128508c31bb3078ef21db81d5d
Rodolfo Aravena Collipal <rodo.aravena@outlook.com>
2024-07-21 14:22:33 -0400

    Se corrigió el problema que había al momento de eliminar un post

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,9 +3,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormControl, NgForm } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
-import { MatDialog } from '@angular/material/dialog'
+import { MatDialog, MatDialogRef, MatDialogModule } from '@angular/material/dialog';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -18,10 +18,11 @@ import Swal from 'sweetalert2';
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'challenge';
@@ -30,84 +31,91 @@ export class AppComponent {
 
   filtro: FormControl;
   publicaciones: any[];
-  encabezados:string[] = [
-    'name',
-    'description',
-    'options'
-  ];
+  encabezados: string[] = ['name', 'description', 'options'];
 
-  constructor(public dialog: MatDialog){
+  constructor(public dialog: MatDialog) {
     this.filtro = new FormControl('');
     this.publicaciones = [];
     this.addPostDialog = {} as TemplateRef<any>;
 
     this.getPosts();
-
   }
 
-  buscar():void {
+  buscar(): void {
     console.log(this.filtro.value);
   }
 
-  async getPosts(){
+  async getPosts() {
     const result = await fetch('http://localhost:5047/posts');
 
     this.publicaciones = (await result.json()) as any[];
-
   }
 
-  showAddPostForm(){
+  showAddPostForm() {
     const dialogForm = this.dialog.open(this.addPostDialog);
+    dialogForm.afterClosed().subscribe((result) => {
+      if (result !== undefined) {
+        if (result !== 'no') {
+          const enabled = 'Y';
+          console.log(result);
+        } else if (result === 'no') {
+          console.log('User clicked no.');
+        }
+      }
+    });
   }
 
-  addPost(){
+  addPost() {}
 
-  }
-
-  warningDeletePost(datos:any){
-
+  warningDeletePost(datos: any) {
     Swal.fire({
       title: '¡Precaución!',
-      text:`Está seguro que desea eliminar "${datos.name}"`,
+      text: `Está seguro que desea eliminar "${datos.name}"`,
       icon: 'warning',
-      confirmButtonText:'Sí, eliminar',
+      confirmButtonText: 'Sí, eliminar',
       confirmButtonColor: 'crimson',
-      showCancelButton:true,
-      cancelButtonText:'Cancelar',
+      showCancelButton: true,
+      cancelButtonText: 'Cancelar',
       loaderHtml: '<mat-spinner></mat-spinner>',
       preConfirm: () => {
-        Swal.showLoading()
+        Swal.showLoading();
       },
     }).then(async (result) => {
-
       const responseStatus = await this.deletePost(datos.id);
 
-      if(responseStatus == 200){
-        Swal.fire('Eliminado', `Se ha eliminado "${datos.name}" correctamente`, 'success');
-      }
-      else{
-        Swal.fire('¡Ups!', `Ha ocurrido un erroral intentar eliminar "${datos.name}"`, 'error');
+      if (responseStatus == 200) {
+        Swal.fire(
+          'Eliminado',
+          `Se ha eliminado "${datos.name}" correctamente`,
+          'success'
+        );
+      } else {
+        Swal.fire(
+          '¡Ups!',
+          `Ha ocurrido un erroral intentar eliminar "${datos.name}"`,
+          'error'
+        );
       }
       this.getPosts();
     });
-
   }
 
-  async deletePost(postId:number): Promise<number>{
-    const result = await fetch('http://localhost:5047/posts',
-      {
-        method: 'DELETE',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-          id: postId,
-        })
-      }
-    );
+  async deletePost(postId: number): Promise<number> {
+    const result = await fetch(`http://localhost:5047/post/${postId}`, {
+      method: 'DELETE',
+    });
     return result.status;
-
   }
 
+  onSend(form: NgForm){
+    if(form.status === 'INVALID')
+    {
+      // display error in your form
+    }else{
+        console.log(form.value)
+        this.dialog.closeAll(); // Close opened diaglo
+      // do whatever you want to do with your data
+    }
+
+  }
 }
