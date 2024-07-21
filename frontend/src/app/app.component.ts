f19807ebdc9b07acc0f6aec07a26a2e9eebe64e4
Rodolfo Aravena Collipal <rodo.aravena@outlook.com>
2024-07-21 00:52:15 -0400

    Se están obteniendo los post de la API

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
+import { MatDialog } from '@angular/material/dialog'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -25,6 +26,8 @@ import { MatTableModule } from '@angular/material/table';
 export class AppComponent {
   title = 'challenge';
 
+  @ViewChild('addPostDialog') addPostDialog: TemplateRef<any>;
+
   filtro: FormControl;
   publicaciones: any[];
   encabezados:string[] = [
@@ -33,23 +36,77 @@ export class AppComponent {
     'options'
   ];
 
-
-
-  constructor(){
+  constructor(public dialog: MatDialog){
     this.filtro = new FormControl('');
     this.publicaciones = [];
+    this.addPostDialog = {} as TemplateRef<any>;
+
+    this.getPosts();
 
   }
 
   buscar():void {
-    console.log(this.filtro.value)
+    console.log(this.filtro.value);
   }
 
-  agregarPublicacion():void{
+  async getPosts(){
+    const result = await fetch('http://localhost:5047/posts');
+
+    this.publicaciones = (await result.json()) as any[];
 
   }
 
-  eliminarPublicacion(datos:any):void{
+  showAddPostForm(){
+    const dialogForm = this.dialog.open(this.addPostDialog);
+  }
+
+  addPost(){
+
+  }
+
+  warningDeletePost(datos:any){
+
+    Swal.fire({
+      title: '¡Precaución!',
+      text:`Está seguro que desea eliminar "${datos.name}"`,
+      icon: 'warning',
+      confirmButtonText:'Sí, eliminar',
+      confirmButtonColor: 'crimson',
+      showCancelButton:true,
+      cancelButtonText:'Cancelar',
+      loaderHtml: '<mat-spinner></mat-spinner>',
+      preConfirm: () => {
+        Swal.showLoading()
+      },
+    }).then(async (result) => {
+
+      const responseStatus = await this.deletePost(datos.id);
+
+      if(responseStatus == 200){
+        Swal.fire('Eliminado', `Se ha eliminado "${datos.name}" correctamente`, 'success');
+      }
+      else{
+        Swal.fire('¡Ups!', `Ha ocurrido un erroral intentar eliminar "${datos.name}"`, 'error');
+      }
+      this.getPosts();
+    });
+
+  }
+
+  async deletePost(postId:number): Promise<number>{
+    const result = await fetch('http://localhost:5047/posts',
+      {
+        method: 'DELETE',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          id: postId,
+        })
+      }
+    );
+    return result.status;
 
   }
 
